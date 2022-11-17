f596c41916b7753f1c6e52c667e2c89e286453ab
Pinho Pinheiro <pinhoou@gmail.com>
2022-11-17 19:39:24 -0300

    Update script.js

--- exSiteRelogio/modelo/script.js
+++ exSiteRelogio/modelo/script.js
@@ -2,21 +2,23 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
+    var minutes = data.getMinutes()
     var hora = data.getHours()
+    var day = data.getDate()
 
-    msg.innerHTML = `Agora são ${hora} horas`
+    msg.innerHTML = `Agora são ${hora} horas e ${minutes} minutos do dia ${day}`
     if (hora >= 0 && hora <= 12) {
-        img.src = '/exSiteRelogio/modelo/arquivos/manha.png'
+        img.src = '/modelo/arquivos/manha.png'
         document.body.style.background = '#fecc42'
     }
 
     else if (hora > 12 && hora < 18) {
-        img.src = '/exSiteRelogio/modelo/arquivos/tarde.png'
+        img.src = '/modelo/arquivos/tarde.png'
         document.body.style.background = '#f46a04'
     }
 
     else {
-        img.src = '/exSiteRelogio/modelo/arquivos/noite.png'
+        img.src = '/modelo/arquivos/noite.png'
         document.body.style.background = '#15232f'
     }
 
